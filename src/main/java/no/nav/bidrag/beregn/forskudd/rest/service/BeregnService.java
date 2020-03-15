fa6af9dd1e42c7245e0125f9a1c911ff2d20f3d4
LarsOttoHaugen <lars.otto.haugen@nav.no>
2020-03-15 16:58:19 +0100

    Koblet på sjablon-consumer for kall av bidrag-sjablon

--- src/main/java/no/nav/bidrag/beregn/forskudd/rest/service/BeregnService.java
+++ src/main/java/no/nav/bidrag/beregn/forskudd/rest/service/BeregnService.java
@@ -2,9 +2,6 @@ package no.nav.bidrag.beregn.forskudd.rest.service;
 
 import static java.util.stream.Collectors.toList;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import no.nav.bidrag.beregn.forskudd.core.ForskuddCore;
 import no.nav.bidrag.beregn.forskudd.core.dto.ForskuddPeriodeGrunnlagDto;
@@ -27,9 +24,8 @@ public class BeregnService {
   }
 
   public BeregnForskuddResultat beregn(ForskuddPeriodeGrunnlagDto coreDto) {
-//    var sjablonResponse = sjablonConsumer.hentSjablontall();
-//    coreDto.setSjablonVerdiListe(mapSjablonVerdier(sjablonResponse.getBody()));
-    coreDto.setSjablontallListe(mapSjablonVerdier(dummySjablonListe()));
+    var sjablonResponse = sjablonConsumer.hentSjablontall();
+    coreDto.setSjablontallListe(mapSjablonVerdier(sjablonResponse.getBody()));
     var resultatFraCore = forskuddCore.beregnForskudd(coreDto);
     return new BeregnForskuddResultat(resultatFraCore);
   }
@@ -38,16 +34,5 @@ public class BeregnService {
     return sjablontallListe.stream().filter(Sjablontall::erGyldigSjablon)
         .map(sTL -> new SjablontallDto(sTL.getTypeSjablon(), new PeriodeDto(sTL.getDatoFom(), sTL.getDatoTom()), sTL.getVerdi()))
         .collect(toList());
-  }
-
-  private static List<Sjablontall> dummySjablonListe() {
-    var sjablontallListe = new ArrayList<Sjablontall>();
-    sjablontallListe.add(new Sjablontall("0005", LocalDate.parse("2000-01-01"), LocalDate.parse("2030-01-01"), BigDecimal.valueOf(1600)));
-    sjablontallListe.add(new Sjablontall("0013", LocalDate.parse("2000-01-01"), LocalDate.parse("2030-01-01"), BigDecimal.valueOf(320)));
-    sjablontallListe.add(new Sjablontall("0033", LocalDate.parse("2000-01-01"), LocalDate.parse("2030-01-01"), BigDecimal.valueOf(270200)));
-    sjablontallListe.add(new Sjablontall("0034", LocalDate.parse("2000-01-01"), LocalDate.parse("2030-01-01"), BigDecimal.valueOf(419700)));
-    sjablontallListe.add(new Sjablontall("0035", LocalDate.parse("2000-01-01"), LocalDate.parse("2030-01-01"), BigDecimal.valueOf(336500)));
-    sjablontallListe.add(new Sjablontall("0036", LocalDate.parse("2000-01-01"), LocalDate.parse("2030-01-01"), BigDecimal.valueOf(61700)));
-    return sjablontallListe;
   }
 }
