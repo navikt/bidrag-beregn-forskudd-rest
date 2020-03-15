d865229ade947a574751c7ea83a6661f859e050f
LarsOttoHaugen <lars.otto.haugen@nav.no>
2020-03-15 16:23:17 +0100

    Liten justering på sjablonverdier

--- src/main/java/no/nav/bidrag/beregn/forskudd/rest/service/BeregnService.java
+++ src/main/java/no/nav/bidrag/beregn/forskudd/rest/service/BeregnService.java
@@ -42,12 +42,12 @@ public class BeregnService {
 
   private static List<Sjablontall> dummySjablonListe() {
     var sjablontallListe = new ArrayList<Sjablontall>();
-    sjablontallListe.add(new Sjablontall("0005", LocalDate.MIN, LocalDate.MAX, BigDecimal.valueOf(1600)));
-    sjablontallListe.add(new Sjablontall("0013", LocalDate.MIN, LocalDate.MAX, BigDecimal.valueOf(320)));
-    sjablontallListe.add(new Sjablontall("0033", LocalDate.MIN, LocalDate.MAX, BigDecimal.valueOf(270200)));
-    sjablontallListe.add(new Sjablontall("0034", LocalDate.MIN, LocalDate.MAX, BigDecimal.valueOf(419700)));
-    sjablontallListe.add(new Sjablontall("0035", LocalDate.MIN, LocalDate.MAX, BigDecimal.valueOf(336500)));
-    sjablontallListe.add(new Sjablontall("0036", LocalDate.MIN, LocalDate.MAX, BigDecimal.valueOf(61700)));
+    sjablontallListe.add(new Sjablontall("0005", LocalDate.parse("2000-01-01"), LocalDate.parse("2030-01-01"), BigDecimal.valueOf(1600)));
+    sjablontallListe.add(new Sjablontall("0013", LocalDate.parse("2000-01-01"), LocalDate.parse("2030-01-01"), BigDecimal.valueOf(320)));
+    sjablontallListe.add(new Sjablontall("0033", LocalDate.parse("2000-01-01"), LocalDate.parse("2030-01-01"), BigDecimal.valueOf(270200)));
+    sjablontallListe.add(new Sjablontall("0034", LocalDate.parse("2000-01-01"), LocalDate.parse("2030-01-01"), BigDecimal.valueOf(419700)));
+    sjablontallListe.add(new Sjablontall("0035", LocalDate.parse("2000-01-01"), LocalDate.parse("2030-01-01"), BigDecimal.valueOf(336500)));
+    sjablontallListe.add(new Sjablontall("0036", LocalDate.parse("2000-01-01"), LocalDate.parse("2030-01-01"), BigDecimal.valueOf(61700)));
     return sjablontallListe;
   }
 }
