4d124f9f38507f2a204832306b1e5b7a07acb0b7
LarsOttoHaugen <lars.otto.haugen@nav.no>
2020-04-28 07:20:32 +0200

    Lagt til correlation-id

--- src/main/java/no/nav/bidrag/beregn/forskudd/rest/BidragBeregnForskudd.java
+++ src/main/java/no/nav/bidrag/beregn/forskudd/rest/BidragBeregnForskudd.java
@@ -2,6 +2,7 @@ package no.nav.bidrag.beregn.forskudd.rest;
 
 import no.nav.bidrag.beregn.forskudd.core.ForskuddCore;
 import no.nav.bidrag.beregn.forskudd.rest.consumer.SjablonConsumer;
+import no.nav.bidrag.commons.web.CorrelationIdFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,6 +20,11 @@ public class BidragBeregnForskudd {
   @Bean
   public SjablonConsumer sjablonConsumer(@Value("${SJABLON_URL}") String sjablonBaseUrl, RestTemplate restTemplate) {
     return new SjablonConsumer(restTemplate, sjablonBaseUrl);
+  }
+
+  @Bean
+  public CorrelationIdFilter correlationIdFilter() {
+    return new CorrelationIdFilter();
   }
 
   public static void main(String[] args) {
