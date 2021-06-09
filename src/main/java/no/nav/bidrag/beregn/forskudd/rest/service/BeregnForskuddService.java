a12e9893f2d4285291a637197313b0b6b8d709a7
LarsOttoHaugen <Norge2355>
2021-06-09 10:28:49 +0200

    Skilt ut mapping til core i egen klasse

--- src/main/java/no/nav/bidrag/beregn/forskudd/rest/service/BeregnForskuddService.java
+++ src/main/java/no/nav/bidrag/beregn/forskudd/rest/service/BeregnForskuddService.java
@@ -1,40 +1,21 @@
 package no.nav.bidrag.beregn.forskudd.rest.service;
 
-import static java.util.Collections.emptyList;
-import static java.util.Collections.singletonList;
 import static java.util.stream.Collectors.toList;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import no.nav.bidrag.beregn.felles.dto.AvvikCore;
-import no.nav.bidrag.beregn.felles.dto.PeriodeCore;
-import no.nav.bidrag.beregn.felles.dto.SjablonInnholdCore;
-import no.nav.bidrag.beregn.felles.dto.SjablonPeriodeCore;
-import no.nav.bidrag.beregn.felles.enums.SjablonInnholdNavn;
-import no.nav.bidrag.beregn.felles.enums.SjablonTallNavn;
 import no.nav.bidrag.beregn.forskudd.core.ForskuddCore;
-import no.nav.bidrag.beregn.forskudd.core.dto.BarnPeriodeCore;
-import no.nav.bidrag.beregn.forskudd.core.dto.BeregnForskuddGrunnlagCore;
 import no.nav.bidrag.beregn.forskudd.core.dto.BeregnetForskuddResultatCore;
-import no.nav.bidrag.beregn.forskudd.core.dto.BostatusPeriodeCore;
-import no.nav.bidrag.beregn.forskudd.core.dto.InntektPeriodeCore;
 import no.nav.bidrag.beregn.forskudd.core.dto.ResultatPeriodeCore;
-import no.nav.bidrag.beregn.forskudd.core.dto.SivilstandPeriodeCore;
-import no.nav.bidrag.beregn.forskudd.core.dto.SoknadBarnCore;
 import no.nav.bidrag.beregn.forskudd.rest.consumer.SjablonConsumer;
-import no.nav.bidrag.beregn.forskudd.rest.consumer.Sjablontall;
 import no.nav.bidrag.beregn.forskudd.rest.dto.http.BeregnForskuddGrunnlag;
 import no.nav.bidrag.beregn.forskudd.rest.dto.http.BeregnetForskuddResultat;
-import no.nav.bidrag.beregn.forskudd.rest.dto.http.Grunnlag;
 import no.nav.bidrag.beregn.forskudd.rest.dto.http.ResultatGrunnlag;
 import no.nav.bidrag.beregn.forskudd.rest.exception.UgyldigInputException;
 import no.nav.bidrag.commons.web.HttpResponse;
@@ -47,13 +28,6 @@ import org.springframework.stereotype.Service;
 public class BeregnForskuddService {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(BeregnForskuddService.class);
-
-  private static final String INNTEKT_TYPE = "Inntekt";
-  private static final String SIVILSTAND_TYPE = "Sivilstand";
-  private static final String BARN_TYPE = "Barn";
-
-  private static final String GENERELL_INFO_TYPE = "GenerellInfo";
-  private static final String BOSTATUS_TYPE = "Bostatus";
 
   private final SjablonConsumer sjablonConsumer;
   private final ForskuddCore forskuddCore;
@@ -68,18 +42,12 @@ public class BeregnForskuddService {
     // Kontroll av inputdata
     grunnlag.valider();
 
-    // Lager en map for sjablontall (id og navn)
-    var sjablontallMap = new HashMap<String, SjablonTallNavn>();
-    for (SjablonTallNavn sjablonTallNavn : SjablonTallNavn.values()) {
-      sjablontallMap.put(sjablonTallNavn.getId(), sjablonTallNavn);
-    }
-
     // Henter sjabloner
     var sjablonSjablontallResponse = sjablonConsumer.hentSjablonSjablontall();
     LOGGER.debug("Antall sjabloner hentet av type Sjablontall: {}", sjablonSjablontallResponse.getResponseEntity().getBody().size());
 
     // Lager input-grunnlag til core-modulen
-    var grunnlagTilCore = mapGrunnlagTilCore(grunnlag, sjablontallMap, sjablonSjablontallResponse.getResponseEntity().getBody());
+    var grunnlagTilCore = CoreMapper.mapGrunnlagTilCore(grunnlag, sjablonSjablontallResponse.getResponseEntity().getBody());
 
     // Kaller core-modulen for beregning av forskudd
     LOGGER.debug("Forskudd - grunnlag for beregning: {}", grunnlagTilCore);
@@ -102,96 +70,6 @@ public class BeregnForskuddService {
     LOGGER.debug("Forskudd - resultat av beregning: {}", resultatFraCore.getBeregnetForskuddPeriodeListe());
     var grunnlagReferanseListe = lagGrunnlagReferanseListe(grunnlag, resultatFraCore);
     return HttpResponse.from(HttpStatus.OK, new BeregnetForskuddResultat(resultatFraCore, grunnlagReferanseListe));
-  }
-
-  private BeregnForskuddGrunnlagCore mapGrunnlagTilCore(BeregnForskuddGrunnlag beregnForskuddGrunnlag,
-      HashMap<String, SjablonTallNavn> sjablontallMap, List<Sjablontall> sjablontallListe) {
-
-    LocalDate soknadBarnFodselsdato = null;
-    String soknadBarnReferanse = null;
-    var bostatusPeriodeCoreListe = new ArrayList<BostatusPeriodeCore>();
-    var inntektPeriodeCoreListe = new ArrayList<InntektPeriodeCore>();
-    var sivilstandPeriodeCoreListe = new ArrayList<SivilstandPeriodeCore>();
-    var barnPeriodeCoreListe = new ArrayList<BarnPeriodeCore>();
-
-    for (Grunnlag grunnlag : beregnForskuddGrunnlag.getGrunnlagListe()) {
-      switch (grunnlag.getType()) {
-        case GENERELL_INFO_TYPE -> {
-          soknadBarnFodselsdato = mapFodselsdato(grunnlag);
-          soknadBarnReferanse = grunnlag.getReferanse();
-        }
-        case BOSTATUS_TYPE -> bostatusPeriodeCoreListe.add(mapBostatus(grunnlag));
-        case INNTEKT_TYPE -> inntektPeriodeCoreListe.add(mapInntekt(grunnlag));
-        case SIVILSTAND_TYPE -> sivilstandPeriodeCoreListe.add(mapSivilstand(grunnlag));
-        case BARN_TYPE -> barnPeriodeCoreListe.add(mapBarn(grunnlag));
-      }
-    }
-
-    var soknadBarnCore = new SoknadBarnCore(soknadBarnReferanse, soknadBarnFodselsdato, bostatusPeriodeCoreListe);
-
-    var sjablonPeriodeCoreListe = mapSjablonVerdier(beregnForskuddGrunnlag.getBeregnDatoFra(), beregnForskuddGrunnlag.getBeregnDatoTil(),
-        sjablontallListe, sjablontallMap);
-
-    return new BeregnForskuddGrunnlagCore(beregnForskuddGrunnlag.getBeregnDatoFra(), beregnForskuddGrunnlag.getBeregnDatoTil(), soknadBarnCore,
-        inntektPeriodeCoreListe, sivilstandPeriodeCoreListe, barnPeriodeCoreListe, sjablonPeriodeCoreListe);
-  }
-
-  private LocalDate mapFodselsdato(Grunnlag grunnlag) {
-    var fodselsdato = Optional.of(grunnlag.getInnhold().get("fodselsdato"))
-        .orElseThrow(() -> new UgyldigInputException("fodselsdato mangler i objekt av type GenerellInfo")).asText();
-    return LocalDate.parse(fodselsdato);
-  }
-
-  private BostatusPeriodeCore mapBostatus(Grunnlag grunnlag) {
-    var bostatusKode = Optional.of(grunnlag.getInnhold().get("bostatusKode"))
-        .orElseThrow(() -> new UgyldigInputException("bostatusKode mangler i objekt av type Bostatus")).asText();
-    return new BostatusPeriodeCore(grunnlag.getReferanse(), mapPeriode(grunnlag.getInnhold(), grunnlag.getType()), bostatusKode);
-  }
-
-  private InntektPeriodeCore mapInntekt(Grunnlag grunnlag) {
-    var inntektType = Optional.of(grunnlag.getInnhold().get("inntektType"))
-        .orElseThrow(() -> new UgyldigInputException("inntektType mangler i objekt av type Inntekt")).asText();
-    var belop = Optional.of(grunnlag.getInnhold().get("belop"))
-        .orElseThrow(() -> new UgyldigInputException("belop mangler i objekt av type Inntekt")).asText();
-    return new InntektPeriodeCore(grunnlag.getReferanse(), mapPeriode(grunnlag.getInnhold(), grunnlag.getType()), inntektType,
-        new BigDecimal(belop));
-  }
-
-  private SivilstandPeriodeCore mapSivilstand(Grunnlag grunnlag) {
-    var sivilstandKode = Optional.of(grunnlag.getInnhold().get("sivilstandKode"))
-        .orElseThrow(() -> new UgyldigInputException("sivilstandKode mangler i objekt av type Sivilstand")).asText();
-    return new SivilstandPeriodeCore(grunnlag.getReferanse(), mapPeriode(grunnlag.getInnhold(), grunnlag.getType()), sivilstandKode);
-  }
-
-  private BarnPeriodeCore mapBarn(Grunnlag grunnlag) {
-    return new BarnPeriodeCore(grunnlag.getReferanse(), mapPeriode(grunnlag.getInnhold(), grunnlag.getType()));
-  }
-
-  private PeriodeCore mapPeriode(JsonNode grunnlagInnhold, String grunnlagType) {
-    var datoFom = Optional.of(grunnlagInnhold.get("datoFom"))
-        .orElseThrow(() -> new UgyldigInputException("datoFom mangler i objekt av type " + grunnlagType)).asText();
-    var datoTil = Optional.of(grunnlagInnhold.get("datoTil")).orElse(null).asText();
-    return new PeriodeCore(LocalDate.parse(datoFom), LocalDate.parse(datoTil));
-  }
-
-  // Plukker ut aktuelle sjabloner og flytter inn i inputen til core-modulen
-  private List<SjablonPeriodeCore> mapSjablonVerdier(LocalDate beregnDatoFra, LocalDate beregnDatoTil, List<Sjablontall> sjablonSjablontallListe,
-      HashMap<String, SjablonTallNavn> sjablontallMap) {
-    return sjablonSjablontallListe
-        .stream()
-        .filter(sjablon -> (!(sjablon.getDatoFom().isAfter(beregnDatoTil)) && (!(sjablon.getDatoTom().isBefore(beregnDatoFra)))))
-        .filter(sjablon -> filtrerSjablonTall(sjablontallMap.getOrDefault(sjablon.getTypeSjablon(), SjablonTallNavn.DUMMY)))
-        .map(sjablon -> new SjablonPeriodeCore(
-            new PeriodeCore(sjablon.getDatoFom(), sjablon.getDatoTom()),
-            sjablontallMap.getOrDefault(sjablon.getTypeSjablon(), SjablonTallNavn.DUMMY).getNavn(),
-            emptyList(),
-            singletonList(new SjablonInnholdCore(SjablonInnholdNavn.SJABLON_VERDI.getNavn(), sjablon.getVerdi()))))
-        .collect(toList());
-  }
-
-  // Sjekker om en type SjablonTall er i bruk for forskudd
-  private boolean filtrerSjablonTall(SjablonTallNavn sjablonTallNavn) {
-    return sjablonTallNavn.getForskudd();
   }
 
   // Lager en liste over resultatgrunnlag som inneholder:
@@ -230,7 +108,7 @@ public class BeregnForskuddService {
   }
 
   // Unngå å legge ut datoer høyere enn 9999-12-31
-  private String mapDato(LocalDate dato) {
+  private static String mapDato(LocalDate dato) {
     return dato.isAfter(LocalDate.parse("9999-12-31")) ? "9999-12-31" : dato.toString();
   }
 }
