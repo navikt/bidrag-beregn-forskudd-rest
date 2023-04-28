6ffc5807bcabf9e60b02c6b511e9b2ff790f0bcc
bent-lorentzen <54444412+bent-lorentzen@users.noreply.github.com>
2023-04-28 12:57:49 +0200

    Spring boot 3 (#154)

--- src/test/java/no/nav/bidrag/beregn/forskudd/rest/controller/BeregnForskuddControllerIntegrationTest.java
+++ src/test/java/no/nav/bidrag/beregn/forskudd/rest/controller/BeregnForskuddControllerIntegrationTest.java
@@ -18,7 +18,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
