1e595cd7c687ff1de494354eddc289a6a7ed269a
Bent J. Lorentzen <bent.lorentzen@nav.no>
2023-04-28 12:36:52 +0200

    Spring boot 3

--- src/test/java/no/nav/bidrag/beregn/forskudd/rest/controller/BeregnForskuddControllerMockTest.java
+++ src/test/java/no/nav/bidrag/beregn/forskudd/rest/controller/BeregnForskuddControllerMockTest.java
@@ -21,7 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
