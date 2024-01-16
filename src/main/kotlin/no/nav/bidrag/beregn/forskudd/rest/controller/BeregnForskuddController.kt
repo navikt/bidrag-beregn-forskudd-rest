2f74d049319b440534a7adfcb9b9163805cb6bee
Ugur Cenar <cenar93@gmail.com>
2024-01-16 15:30:09 +0100

    Bruk beregnin api fra felles repo og fjern overført kode

--- src/main/kotlin/no/nav/bidrag/beregn/forskudd/rest/controller/BeregnForskuddController.kt
+++ src/main/kotlin/no/nav/bidrag/beregn/forskudd/rest/controller/BeregnForskuddController.kt
@@ -1,12 +1,14 @@
 package no.nav.bidrag.beregn.forskudd.rest.controller
 
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.ExampleObject
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
-import no.nav.bidrag.beregn.forskudd.rest.service.BeregnForskuddService
+import io.swagger.v3.oas.models.examples.Example
+import no.nav.bidrag.beregn.forskudd.BeregnForskuddApi
 import no.nav.bidrag.transport.behandling.beregning.felles.BeregnGrunnlag
-import no.nav.bidrag.transport.behandling.beregning.forskudd.BeregnetForskuddResultat
 import no.nav.security.token.support.core.api.Protected
-import org.springframework.http.ResponseEntity
+import org.springframework.context.annotation.Bean
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,12 +17,31 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/beregn")
 @Protected
-class BeregnForskuddController(private val beregnForskuddService: BeregnForskuddService) {
+class BeregnForskuddController(private val beregning: BeregnForskuddApi) {
+    @Bean
+    fun forskuddExample(): Example {
+        val example = Example()
+        example.value = BeregnForskuddController::class.java.getResource("/eksempler/beregn_request.json")?.readText() ?: ""
+        example.description = "Forskudd beregning"
+        return example
+    }
+
     @PostMapping(path = ["/forskudd"])
     @Operation(summary = "Beregner forskudd")
     @SecurityRequirement(name = "bearer-key")
-    fun beregnForskudd(@RequestBody beregnForskuddGrunnlag: BeregnGrunnlag): ResponseEntity<BeregnetForskuddResultat> {
-        val resultat = beregnForskuddService.beregn(beregnForskuddGrunnlag)
-        return ResponseEntity(resultat.responseEntity.body, resultat.responseEntity.statusCode)
-    }
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+        content =
+        [
+            Content(
+                examples =
+                [
+                    ExampleObject(
+                        ref = "#/components/examples/Forskudd beregning",
+                        name = "Forskudd",
+                    ),
+                ],
+            ),
+        ],
+    )
+    fun beregnForskudd(@RequestBody beregnForskuddGrunnlag: BeregnGrunnlag) = beregning.beregn(beregnForskuddGrunnlag)
 }
